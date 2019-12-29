dc73fdf3108350af6dfcd3a4f066d4809e559791
Sune Gynthersen <sune.gynthersen@gmail.com>
2019-12-29 11:24:38 +0100

    Check for already used letters

--- test/game_test.exs
+++ test/game_test.exs
@@ -11,4 +11,25 @@ defmodule GameTest do
     assert length(game.letters) > 0
     assert game.letters == game.letters |> List.to_string |> String.downcase |> String.codepoints
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  test "First occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "Second occurence of letter is already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
 end
