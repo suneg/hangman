d275c36e162c61ab5183e26f450a1798a8fe9ab4
Sune Gynthersen <sune.gynthersen@gmail.com>
2019-12-29 11:46:05 +0100

    Test that guessing the whole word wins the game

--- test/game_test.exs
+++ test/game_test.exs
@@ -32,4 +32,22 @@ defmodule GameTest do
     { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
+
+  test "a good guess is recognized" do 
+    game = Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do 
+    game = Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
+    { game, _tally } = Game.make_move(game, "i")
+    { game, _tally } = Game.make_move(game, "b")
+    { game, _tally } = Game.make_move(game, "l")
+    { game, _tally } = Game.make_move(game, "e")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
 end
