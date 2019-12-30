8097e025cfbd92e9dc5afec878498776f37f627b
Sune Gynthersen <sune.gynthersen@gmail.com>
2019-12-30 16:27:57 +0100

    Keep state in a process using recursion

--- procs.exs
+++ procs.exs
@@ -1,10 +1,12 @@
 defmodule Procs do
-  def greeter(what_to_say) do
+  def greeter(count) do
 
     receive do
+      :reset -> greeter(0)
+      {:add, n } -> greeter(count+n)
       msg ->
-        IO.puts("#{what_to_say} #{msg}")
+        IO.puts("#{count}: Hello #{msg}")
+        greeter(count)
     end
-    greeter(what_to_say)
   end
 end
