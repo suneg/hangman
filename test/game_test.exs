8d4356b3c053535aa41ba4d8c22e6844d2810607
Sune Gynthersen <sune.gynthersen@gmail.com>
2019-12-30 10:30:40 +0100

    Extract tally creation to api

--- test/game_test.exs
+++ test/game_test.exs
@@ -15,53 +15,53 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [ :won, :lost ] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert { ^game, _ } = Game.make_move(game, "x")
+      assert ^game = Game.make_move(game, "x")
     end
   end
 
   test "First occurence of letter is not already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "Second occurence of letter is already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    { game, _tally } = Game.make_move(game, "w")
+    game = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a bad guess is recognized" do
     game = Game.new_game("wibble")
-    { game, _tally } = Game.make_move(game, "q")
+    game = Game.make_move(game, "q")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
   test "lost game is recognized wrong" do
     game = Game.new_game("w")
-    { game, _tally } = Game.make_move(game, "a")
+    game = Game.make_move(game, "a")
     assert game.turns_left == 6
-    { game, _tally } = Game.make_move(game, "b")
+    game = Game.make_move(game, "b")
     assert game.turns_left == 5
-    { game, _tally } = Game.make_move(game, "c")
+    game = Game.make_move(game, "c")
     assert game.turns_left == 4
-    { game, _tally } = Game.make_move(game, "d")
+    game = Game.make_move(game, "d")
     assert game.turns_left == 3
-    { game, _tally } = Game.make_move(game, "e")
+    game = Game.make_move(game, "e")
     assert game.turns_left == 2
-    { game, _tally } = Game.make_move(game, "f")
+    game = Game.make_move(game, "f")
     assert game.turns_left == 1
-    { game, _tally } = Game.make_move(game, "g")
+    game = Game.make_move(game, "g")
     assert game.game_state == :lost
   end
 
@@ -78,7 +78,7 @@ defmodule GameTest do
     game = Game.new_game("wibble")
 
     Enum.reduce(moves, game, fn ({guess, state}, acc) ->
-      { new_state, _ } = Game.make_move(acc, guess)
+      new_state = Game.make_move(acc, guess)
       assert new_state.game_state == state
       new_state
     end)
