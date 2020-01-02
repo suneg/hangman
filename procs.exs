b1c9b97e9d1fc4a33eaa83ba82689e3b736eaab0
Sune Gynthersen <sune.gynthersen@gmail.com>
2020-01-02 11:43:23 +0100

    Experiments with processes

--- procs.exs
+++ procs.exs
@@ -2,6 +2,8 @@ defmodule Procs do
   def greeter(count) do
 
     receive do
+      { :boom, reason } ->
+        exit(reason)
       :reset -> greeter(0)
       {:add, n } -> greeter(count+n)
       msg ->
@@ -10,3 +12,8 @@ defmodule Procs do
     end
   end
 end
+
+
+# inner_process = fn -> Process.sleep(10_000) end
+# outer_process = fn -> spawn(inner_process); exit(:bad) end
+# Enum.each(1..100, fn -> spawn(outer_process) end)
