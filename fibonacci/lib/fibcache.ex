a46191176d941d5ebe3c52ba3c00ba0d0c52ea02
Sune Gynthersen <sune.gynthersen@gmail.com>
2020-01-02 15:20:48 +0100

    A bit of cleanup

--- fibonacci/lib/fibcache.ex
+++ fibonacci/lib/fibcache.ex
@@ -7,17 +7,21 @@ defmodule FibCache do
   end
 
   def lookup(cache, n) do
-    Agent.get(cache, fn x -> x[n] end)
-    |> compute_and_cache_if_missing(cache, n)
+    lookup_in_cache(cache, n)
+    |> compute_if_not_in_cache(cache, n)
   end
 
-  defp compute_and_cache_if_missing(value, cache, n) do
-    cond do
-      value != nil -> value
-      value == nil ->
-        value = lookup(cache, n-1) + lookup(cache, n-2)
-        Agent.get_and_update(cache, fn x -> {x, Map.put(x, n, value)} end)
-        value
-    end
+  defp lookup_in_cache(cache, n) do
+    Agent.get(cache, fn x -> x[n] end)
+  end
+
+  defp compute_if_not_in_cache(nil, cache, n) do
+    value = lookup(cache, n-1) + lookup(cache, n-2)
+    Agent.get_and_update(cache, fn x -> {x, Map.put(x, n, value)} end)
+    value
+  end
+
+  defp compute_if_not_in_cache(value, _, _) do
+    value
   end
 end
--- fibonacci/test/fibonacci_test.exs
+++ fibonacci/test/fibonacci_test.exs
@@ -6,5 +6,7 @@ defmodule FibonacciTest do
     assert Fibonacci.fib(5) == 5
     assert Fibonacci.fib(10) == 55
     assert Fibonacci.fib(15) == 610
+    assert Fibonacci.fib(50) == 12586269025
+    assert Fibonacci.fib(100) == 354224848179261915075
   end
 end
