3e75c9917c556461828293df650533ef4845666c
Sune Gynthersen <sune.gynthersen@gmail.com>
2020-01-04 21:40:38 +0100

    Use Hangman server when calling the API

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -64,7 +64,9 @@ defmodule Hangman.Game do
   end
 
   defp score_guess(game = %{ turns_left: 1}, _not_good_guess) do
-    Map.put(game, :game_state, :lost)
+    game
+    |> Map.put(:game_state, :lost)
+    |> Map.put(:turns_left, 0)
   end
 
   defp score_guess(game = %{ turns_left: turns_left}, _not_good_guess) do
